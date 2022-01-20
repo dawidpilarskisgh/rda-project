a581883c7766dd186ad2b3f5cfcbccd1e1b9b856
dawidpilarskisgh <91250294+dawidpilarskisgh@users.noreply.github.com>
2022-01-20 23:05:56 +0100

    Update twitter_data.py

--- twitter_data.py
+++ twitter_data.py
@@ -3,22 +3,21 @@ import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
 from textblob import TextBlob
-
-# Token to connect to twitter API
-API_key = ''
-API_key_secret = ''
-access_token = ''
-access_token_secret = ''
+import json
 
 def connect_twitter():
-    auth = tweepy.OAuthHandler(API_key, API_key_secret)
-    auth.set_access_token(access_token, access_token_secret)
+    with open('credentials.json', 'r') as c:
+        credentials = json.load(c)
+    auth = tweepy.OAuthHandler(credentials['API_key'], credentials['API_key_secret'] )
+    auth.set_access_token(credentials['access_token'], credentials['access_token_secret'])
     api = tweepy.API(auth)
     return api
 
+
 def search_twitts(api, text_to_search):
-    tweets = api.search_tweets(text_to_search, count=250, lang='en', tweet_mode='extended')
+    tweets = api.search_tweets(text_to_search, count=250, lang='en', result_type = 'mixed', tweet_mode='extended')
     return tweets
+
 
 def create_df_from_twitts(found_tweets):
     df = pd.DataFrame(data=[tweet.full_text for tweet in found_tweets], columns=['original_Tweets'])
@@ -27,27 +26,26 @@ def create_df_from_twitts(found_tweets):
     df['Date'] = pd.to_datetime(df.Date).dt.tz_localize(None)
     df['Likes'] = np.array([tweet.favorite_count for tweet in found_tweets])
     df['RTs'] = np.array([tweet.retweet_count for tweet in found_tweets])
-    
-    #sentiment analysis
+
     sentiment_objects = [TextBlob(tweet.full_text) for tweet in found_tweets]
     sentiment_values = [[tweet.sentiment.polarity, str(tweet)] for tweet in sentiment_objects]
     sentiment_df = pd.DataFrame(sentiment_values, columns=["polarity", "tweet"])
     df['sentiment'] = sentiment_df['polarity']
-    
-    # Filter data to last hour
+
+    #Filter data to last hour
     start_date = datetime.now() - timedelta(hours=2)
     df_latest_data = df[(df['Date'] > start_date)]
+
     return df_latest_data
 
-WORDS_TO_SEARCH = ['bitcoin', 'crypto', 'cryptonews', 'cryptocurrency', 'cryptocurrencies', 'BTC', 'bitmark', 'binance', 'bitbay', 'blockchain']
 
-all_tweets = pd.DataFrame()
+def get_data_from_API(WORDS_TO_SEARCH):
+    all_tweets = pd.DataFrame()
+    connection = connect_twitter()
+    for word in WORDS_TO_SEARCH:
+        found_tweets = search_twitts(connection, word)
+        df_tweet = create_df_from_twitts(found_tweets)
+        all_tweets = all_tweets.append(df_tweet, ignore_index=True)
 
-for word in WORDS_TO_SEARCH:
-    found_tweets = search_twitts(connect_twitter(), word)
-    df_tweet = create_df_from_twitts(found_tweets)
-    frames = [all_tweets, df_tweet]
-    all_tweets = pd.concat(frames)
-
-# print(all_tweets)
-all_tweets.to_csv('bitcoin_data.csv')
+    all_tweets.to_csv('bitcoin_data_0121.csv')
+    return all_tweets
