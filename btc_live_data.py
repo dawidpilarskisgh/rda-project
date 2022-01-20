d25e2bd4d29ec44887929418e57b8b45b3d96153
dawidpilarskisgh <91250294+dawidpilarskisgh@users.noreply.github.com>
2022-01-20 23:04:50 +0100

    Update btc_live_data.py

--- btc_live_data.py
+++ btc_live_data.py
@@ -1,9 +1,17 @@
 import yfinance as yf
+import pandas as pd
 
-period = '2h'
-interval = '1h'
+def get_stock_data(period, interval):
+    btc = yf.download(tickers='BTC-USD', period = period, interval = interval)
+    btc.reset_index(inplace=True)
+    btc = btc[['Datetime', 'Adj Close', 'Volume']]
+    btc['Hours_tmp'] = btc.loc[:,'Datetime']
+    times_stock_d = pd.to_datetime(btc.Datetime)
+    times_stock_h = pd.to_datetime(btc.Hours_tmp)
+    btc['Date'] = times_stock_d.dt.date
+    btc['Hours'] = times_stock_h.dt.hour
+    btc['Minutes'] = times_stock_h.dt.minute
+    df_stock = btc[['Date', 'Hours', 'Minutes','Adj Close']]
+    df_stock.to_csv('btc_data.csv')
+    return df_stock
 
-btc = yf.download(tickers='BTC-USD', period = period, interval = interval)
-
-#print(btc)
-btc.to_csv('btc_data.csv')
