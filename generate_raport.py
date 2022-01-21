e7aab96e62fc09d0f42202f1f2be13eb9e24c8da
dawidpilarskisgh <91250294+dawidpilarskisgh@users.noreply.github.com>
2022-01-21 23:35:32 +0100

    Update generate_raport.py

--- generate_raport.py
+++ generate_raport.py
@@ -1,32 +1,31 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 
-df = pd.read_csv('data/prediction_data.csv')
+def make_plot(data):
+    df = data
+    prediction_data = np.array(df.iloc[:, :11])
 
-prediction_data = np.array(df.iloc[:, 1:12])
-model_type = np.array(df.iloc[:, 12:13])
+    linear_blob = []
+    linear_nltk = []
+    rfr_blob = []
+    rfr_nltk = []
 
-linear = []
-rfr = []
-for i in range(0,len(prediction_data)):
-    if i < len(prediction_data)/2:
-        linear.append(prediction_data[i])
-    else:
-        rfr.append(prediction_data[i])
+    linear_blob.append(prediction_data[0])
+    rfr_blob.append(prediction_data[1])
+    linear_nltk.append(prediction_data[2])
+    rfr_nltk.append(prediction_data[3])
 
-test_data = np.array(df.iloc[:, 15:26])
+    col = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55]
 
-
-
-col = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55]
-
-for i in range(0, len(linear)):
-    plt.plot(col, linear[i], label = 'Dane predykcyjne - model: regresja liniowa')
-    plt.plot(col, rfr[i], label='Dane predykcyjne - model: las losowy')
-    plt.plot(col, test_data[i], label = 'Dane rzeczywiste')
+    plt.plot(col, linear_blob[0], label='Predykcja- model: RegL, sent:TB')
+    plt.plot(col, rfr_blob[0], label='Predykcja- model: LasL, sent:TB')
+    plt.plot(col, linear_nltk[0], label='Predykcja- model: RegL, sent: NLTK')
+    plt.plot(col, rfr_nltk[0], label='Predykcja- model: LasL, sent:NLTK')
     plt.xlabel("Wartość w czasie - jedna godzina (przedział 5 minutowy)")
     plt.ylabel("Kurs BTC (USD)")
-    plt.title("Wykres danych predykcyjnych vs dane rzeczywiste")
-    plt.legend()
+    plt.title("Wykres danych predykcyjnych")
+    box = plt.subplot(111).get_position()
+    plt.subplot(111).set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     plt.show()
+
